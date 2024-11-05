c08f2adba113fbc0968b9e9f0d85c6dfb88a71df
Jaysquare <jamiuolajide20@gmail.com>
2024-11-05 08:58:12 -0600

    message generator project

---
+++ messageGen.js
@@ -0,0 +1,21 @@
+const random = Math.round(Math.random()*4);
+
+
+switch (random){
+    case 0:
+     console.log("The best use of life is LOVE");
+     break;
+
+    case 1:
+     console.log("The best expression of love is TIME");
+     break;
+
+    case 3:
+     console.log("The best time to love is NOW");
+     break;
+
+    case 4:
+     console.log("There is a purpose behind every situation( problem and fortune)");
+     break;
+     
+}
