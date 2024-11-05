1997a040600f94c9aaf73d605eb5d42470bdbdfa
Jaysquare <jamiuolajide20@gmail.com>
2024-11-05 11:10:33 -0600

    message generator modification

--- messageGen.js
+++ messageGen.js
@@ -1,21 +1,36 @@
-const random = Math.round(Math.random()*4);
+console.log ("LIST OF MESSAGES")
+console.log("-----------------------------------")
+let array =["The best use of life is LOVE", 
+            "The best expression of love is TIME",
+            "The best time to love is NOW",
+            "There is a purpose behind every situation( problem and fortune)."  ];
 
+for(let i=0; i < array.length; i++)
+  console.log(array[i]);
 
-switch (random){
+console.log("")
+console.log("THE RANDOM MESSAGE")
+console.log("-----------------------------------")
+const messageGen= () => {
+   let random = Math.round(Math.random()*3);
+   switch (random){
     case 0:
-     console.log("The best use of life is LOVE");
+     return "The best use of life is LOVE";
      break;
 
     case 1:
-     console.log("The best expression of love is TIME");
+     return "The best expression of love is TIME";
+     break;
+
+    case 2:
+     return"The best time to love is NOW";
      break;
 
     case 3:
-     console.log("The best time to love is NOW");
+     return "There is a purpose behind every situation( problem and fortune)";
      break;
+    
+  }
+};
 
-    case 4:
-     console.log("There is a purpose behind every situation( problem and fortune)");
-     break;
-     
-}
+console.log(messageGen());
